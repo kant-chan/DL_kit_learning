cb0a733411936c28267753a5b2a47adce0f57af8
muzhi <kantchenjn@163.com>
2019-02-28 17:53:42 +0800

    convolutional autoencoder and add noise

--- keras_kit/autoencoder.py
+++ keras_kit/autoencoder.py
@@ -1,7 +1,8 @@
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 from keras.layers import Input, Dense
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.datasets import mnist
 
 LOG_DIR = os.path.join('./', 'log')
@@ -11,45 +12,98 @@ def model_path(basepath, filename):
         os.makedirs(basepath)
     return os.path.join(basepath, filename)
 
-encoding_dim = 32  # 784/32 = factor 24.5
 
-input_img = Input(shape=(784,))
-encoded = Dense(encoding_dim, activation='relu')(input_img)
-decoded = Dense(784, activation='sigmoid')(encoded)
+class AutoEncoder(object):
+    def __init__(self, encoding_dim, in_dim):
+        self.encoding_dim = encoding_dim  # 784/32 = factor 24.5
+        self.in_dim = in_dim
 
-# autoencoder Model
-autoencoder = Model(input_img, decoded)
+    def build(self):
+        input_img = Input(shape=(self.in_dim,))
+        encoded = Dense(self.encoding_dim, activation='relu')(input_img)
+        ## if Adding a sparsity constraint on the encoded representations
+        # add a Dense layer with a L1 activity regularizer
+        # encoded = Dense(encoding_dim, activation='relu',
+        #         activity_regularizer=regularizers.l1(10e-5))(input_img)
+        decoded = Dense(self.in_dim, activation='sigmoid')(encoded)
 
-# encoder Model
-encoder = Model(input_img, encoded)
+        # autoencoder Model
+        autoencoder = Model(input_img, decoded)
 
-# decoder Model
-encoded_input = Input(shape=(encoding_dim,))
-decoder_layer = autoencoder.layers[-1]
-decoder = Model(encoded_input, decoder_layer(encoded_input))
+        # encoder Model
+        encoder = Model(input_img, encoded)
 
-autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+        # decoder Model
+        encoded_input = Input(shape=(self.encoding_dim,))
+        decoder_layer = autoencoder.layers[-1]
+        decoder = Model(encoded_input, decoder_layer(encoded_input))
+        return autoencoder, encoder, decoder
+    
+    def train(self, model, train_data, test_data):
+        model.compile(optimizer='adadelta', loss='binary_crossentropy')
+        model.fit(train_data, train_data,
+                        epochs=50,
+                        batch_size=256,
+                        shuffle=True,
+                        validation_data=(test_data, test_data))
 
-# load data
-(x_train, _), (x_test, _) = mnist.load_data()
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
+        print("save weights to:", model_path(LOG_DIR, 'autoencoder.h5'))
+        # save whole model not only weights
+        model.save(model_path(LOG_DIR, 'autoencoder.h5'))   
 
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
-print("{:7}: {}".format('x_train', x_train.shape))
-print("{:7}: {}".format('x_test', x_test.shape))
+def data_gen():
+    # load data
+    (x_train, _), (x_test, _) = mnist.load_data()
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255.
 
-autoencoder.fit(x_train, x_train,
-                epochs=50,
-                batch_size=256,
-                shuffle=True,
-                validation_data=(x_test, x_test))
+    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
-print("save weights to:", model_path(LOG_DIR, 'autoencoder.h5'))
-autoencoder.save(model_path(LOG_DIR, 'autoencoder.h5'))   # HDF5 file, you have to pip3 install h5py if don't have it
+    print("{:7}: {}".format('x_train', x_train.shape))
+    print("{:7}: {}".format('x_test', x_test.shape))
+    return x_train, x_test
 
-encoded_img = encoder.predict(x_test)
-decoded_img = decoder.predict(encoded_img)
 
+
+if __name__ == '__main__':
+    train_data, test_data = data_gen()
+    #########
+    # train
+    #########
+    # model = AutoEncoder(32, 784)
+    # autoencoder, encoder, decoder = model.build()
+
+    #########
+    # predict
+    # from saved model
+    #########
+    ## method 1
+    autoencoder = load_model(model_path(LOG_DIR, 'autoencoder.h5'))
+    print(autoencoder.layers)
+    print(autoencoder.inputs)
+    print(autoencoder.outputs)
+    decoded_img = autoencoder.predict(test_data)
+
+    ## method 2
+    # TODO
+    # encoded_img = encoder.predict(x_test)
+    # decoded_img = decoder.predict(encoded_img)
+
+    n = 5
+    plt.figure(figsize=(10, 4))
+    for i in range(n):
+        ax = plt.subplot(2, n, i + 1)
+        plt.imshow(test_data[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        ax = plt.subplot(2, n, i + 1 + n)
+        plt.imshow(decoded_img[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        
+    plt.show()
---
+++ keras_kit/convolutional_autoencoder.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import os
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.models import Model
+from keras import backend as K
+from keras.callbacks import TensorBoard
+from keras.datasets import mnist
+
+LOG_DIR = os.path.join('./', 'log')
+
+def model_path(basepath, filename):
+    if not os.path.exists(basepath):
+        os.makedirs(basepath)
+    return os.path.join(basepath, filename)
+
+
+def data_gen():
+    (x_train, _), (x_test, _) = mnist.load_data()
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255.
+    x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+    x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+    return x_train, x_test
+
+
+def add_noise(data, f):
+    noise_factor = f  # example f = 0.5
+    x_train_noisy = data + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=data.shape)
+    x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+    return x_train_noisy
+
+
+##### add noise, net construct is: #####
+
+# input_img = Input(shape=(28, 28, 1))
+# x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
+# x = MaxPooling2D((2, 2), padding='same')(x)
+# x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+# encoded = MaxPooling2D((2, 2), padding='same')(x)
+# # at this point the representation is (7, 7, 32)
+# x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
+# x = UpSampling2D((2, 2))(x)
+# x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+# x = UpSampling2D((2, 2))(x)
+# decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+#####
+
+class ConvAutoencoder(object):
+    
+    def build(self):
+        input_img = Input(shape=(28, 28, 1))
+
+        x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+        x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+        x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+        encoded = MaxPooling2D((2, 2), padding='same')(x)
+        # at this point the representation is (4, 4, 8) i.e. 128-dimensional
+        x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+        x = UpSampling2D((2, 2))(x)
+        x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+        x = UpSampling2D((2, 2))(x)
+        x = Conv2D(16, (3, 3), activation='relu')(x)
+        x = UpSampling2D((2, 2))(x)
+        decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+        self.autoencoder_model = Model(input_img, decoded)
+
+    def train(self, train_data, train_label, test_data, test_label):
+        self.autoencoder_model.compile(optimizer='adadelta', loss='binary_crossentropy')
+        self.autoencoder_model.fit(train_data, train_label,
+                                   epochs=50,
+                                   batch_size=128,
+                                   shuffle=True,
+                                   validation_data=(test_data, test_label),
+                                   callbacks=[TensorBoard(log_dir=LOG_DIR)])
+
+        print("save weights to:", model_path(LOG_DIR, 'convolutional_autoencoder.h5'))
+        self.autoencoder_model.save_weights(model_path(LOG_PATH, 'convolutional_autoencoder.h5'))
+        print('training completed!')
+
+    def inference(self, x_test):
+        self.autoencoder_model.load_weights(model_path(LOG_PATH, 'convolutional_autoencoder.h5'))
+        decoded_img = autoencoder.predict(test_data)
+        # visualize results
+        n = 5
+        plt.figure(figsize=(10, 4))
+        for i in range(n):
+            ax = plt.subplot(2, n, i + 1)
+            plt.imshow(test_data[i].reshape(28, 28))
+            plt.gray()
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+
+            ax = plt.subplot(2, n, i + 1 + n)
+            plt.imshow(decoded_img[i].reshape(28, 28))
+            plt.gray()
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+            
+        plt.show()
+
+if __name__ == '__main__':
+    train_data, test_data = data_gen()
+    model = ConvAutoencoder()
+    model.build()
+    model.train(train_data, train_data, test_data, test_data)
